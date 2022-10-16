fc72f22ebad190060760fe1db7300b55911d6f93
Alex Silva <alesimadev@gmail.com>
2022-10-16 11:24:42 -0300

    remove unused from context_processors

--- apps/notification/context_processors.py
+++ apps/notification/context_processors.py
@@ -1,6 +1,3 @@
-from .models import Notification
-
-
 def notifications(request):
     if request.user.is_authenticated:
         return {'notifications': request.user.notifications.filter(is_read=False)}
